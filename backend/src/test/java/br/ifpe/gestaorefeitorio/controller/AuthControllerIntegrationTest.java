dad4b8e1573ecf22fac854a977e8e122b44e65e8
KauanneC <kauannecavalcante5@gmail.com>
2026-09-22 01:13:09 -0300

    fix(ci): unblock develop deployment pipeline

--- backend/src/test/java/br/ifpe/gestaorefeitorio/controller/AuthControllerIntegrationTest.java
+++ backend/src/test/java/br/ifpe/gestaorefeitorio/controller/AuthControllerIntegrationTest.java
@@ -8,9 +8,9 @@ import br.ifpe.gestaorefeitorio.security.GoogleTokenValidator;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -45,7 +45,7 @@ class AuthControllerIntegrationTest {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    @MockBean
+    @MockitoBean
     private GoogleTokenValidator googleTokenValidator;
 
     private GoogleIdToken.Payload payloadComEmail(String email) {
--- backend/src/test/java/br/ifpe/gestaorefeitorio/controller/MovimentacaoControllerIntegrationTest.java
+++ backend/src/test/java/br/ifpe/gestaorefeitorio/controller/MovimentacaoControllerIntegrationTest.java
@@ -14,7 +14,7 @@ import br.ifpe.gestaorefeitorio.repository.UsuarioRepository;
 import br.ifpe.gestaorefeitorio.security.JwtService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.MediaType;
